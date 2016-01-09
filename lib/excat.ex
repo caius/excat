2756b7b394b62bafa47c2dccf7005d9112b27fd3
Caius Durling <dev@caius.name>
2016-01-09 21:22:37 +0000

    Handle exit codes as well
    Along with some internal refactoring

--- lib/excat.ex
+++ lib/excat.ex
@@ -1,22 +1,39 @@
 defmodule Excat do
   # No arguments passed means read from stdin
   def main([]) do
-    copy_from(:stdio)
+    main(["-"])
   end
 
   # Arguments get treated as paths to files, and we ignore stdin
   def main(args) do
-    Enum.each(args, fn path -> 
-      {:ok, handler} = File.open(path, [:read])
-
-      case copy_from(handler) do
-        :ok ->
-          File.close(handler)
-        
-        {:error, reason} ->
-          IO.puts :stderr, "excat: #{path}: No such file or directory"
+    error = Enum.reduce(args, false, fn path, acc ->
+      if path == "-" || File.exists?(path) do
+        read_path(path)
+        acc
+      else
+        output_error(path)
+        true
       end
     end)
+
+    exit_code = if error, do: 1, else: 0
+    exit({:shutdown, exit_code})
+  end
+
+  def read_path("-") do
+    copy_from(:stdio)
+  end
+
+  def read_path(path) do
+    {:ok, h} = File.open(path, [:read])
+
+    case copy_from(h) do
+      :ok ->
+        File.close(h)
+
+      {:error, _} ->
+        output_error(path)
+    end
   end
 
   # Given a source, read a line from it and write to stdout
@@ -28,6 +45,10 @@ defmodule Excat do
         IO.binwrite(data)
         copy_from(source)
     end
+  end
+
+  defp output_error(path) do
+    IO.puts :stderr, "cat: #{path}: No such file or directory"
   end
 
 end
