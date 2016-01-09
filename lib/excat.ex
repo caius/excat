f33ed7f57d30b7aa7d90dc5600e6400ea58da7f1
Caius Durling <dev@caius.name>
2016-01-09 17:12:08 +0000

    Refactor the implementation slightly

--- lib/excat.ex
+++ lib/excat.ex
@@ -1,36 +1,33 @@
 defmodule Excat do
+  # No arguments passed means read from stdin
   def main([]) do
-    copy_stdin
+    copy_from(:stdio)
   end
 
+  # Arguments get treated as paths to files, and we ignore stdin
   def main(args) do
-    Enum.each(args, &Excat.copy_file/1)
+    Enum.each(args, fn path -> 
+      {:ok, handler} = File.open(path, [:read])
+
+      case copy_from(handler) do
+        :ok ->
+          File.close(handler)
+        
+        {:error, reason} ->
+          IO.puts :stderr, "excat: #{path}: No such file or directory"
+      end
+    end)
   end
 
-  def copy_file(filename) do
-    case File.read(filename) do
-      {:ok, content} ->
-        IO.write content
-
-      {:error, reason} ->
-        error(filename, reason)
-
-    end
-  end
-
-  defp copy_stdin do
-    case IO.read(:stdio, :line) do
+  # Given a source, read a line from it and write to stdout
+  defp copy_from(source) do
+    case IO.binread(source, :line) do
       :eof -> :ok
-      {:error, reason} ->
-        error("-", reason)
+      {:error, reason} -> {:error, reason}
       data ->
-        IO.write(data)
-        copy_stdin
+        IO.binwrite(data)
+        copy_from(source)
     end
-  end
-
-  defp error(filename, _) do
-    IO.puts :stderr, "excat: #{filename}: No such file or directory"
   end
 
 end
