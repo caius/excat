348a19801d4f606d8fd2f0b04fb3e04d76aafe26
Caius Durling <dev@caius.name>
2016-01-09 12:36:34 +0000

    Working implementation

--- lib/excat.ex
+++ lib/excat.ex
@@ -1,2 +1,36 @@
 defmodule Excat do
+  def main([]) do
+    copy_stdin
+  end
+
+  def main(args) do
+    Enum.each(args, &Excat.copy_file/1)
+  end
+
+  def copy_file(filename) do
+    case File.read(filename) do
+      {:ok, content} ->
+        IO.write content
+
+      {:error, reason} ->
+        error(filename, reason)
+
+    end
+  end
+
+  defp copy_stdin do
+    case IO.read(:stdio, :line) do
+      :eof -> :ok
+      {:error, reason} ->
+        error("-", reason)
+      data ->
+        IO.write(data)
+        copy_stdin
+    end
+  end
+
+  defp error(filename, _) do
+    IO.puts :stderr, "excat: #{filename}: No such file or directory"
+  end
+
 end
